56145c958ab0b3c1c70565527c2c2e8ef1a5b082
ToastXC <100072983+toastxc@users.noreply.github.com>
2024-06-17 20:49:58 +0800

    upload files

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+use std::time::SystemTime;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct OrderPayload {
+    pub coffee: u8,
+    pub milk: u8,
+    pub temp: u8,
+}
+
+pub mod env;
+
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct OrderInfo {
+    pub order_no: u8,
+    pub coffee_info: OrderPayload,
+    pub date: SystemTime,
+}
