f1e26039312bd90289df562aedc7b3bcedc405b4
ToastXC <100072983+toastxc@users.noreply.github.com>
2024-08-15 10:19:22 +0800

    moved data

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,19 @@
-use std::time::SystemTime;
 use serde::{Deserialize, Serialize};
+use std::time::SystemTime;
 
 #[derive(Serialize, Deserialize, Clone)]
-pub struct OrderPayload {
-    pub coffee: u8,
-    pub milk: u8,
-    pub temp: u8,
+pub enum OrderPayload {
+    Standard { coffee: usize, milk: usize, temp: usize, sugar: usize },
+    Beth(String),
 }
-
-
 #[derive(Serialize, Deserialize, Clone)]
 pub struct OrderInfo {
-    pub order_no: u8,
+    pub order_no: String,
     pub coffee_info: OrderPayload,
     pub date: SystemTime,
 }
+
+pub const COFFEE_TYPE: [&str; 8] = ["Espresso", "Latte", "Flat White", "Cappuccino", "Mocha", "Americano", "Breve", "Con Panna"];
+pub const MILK_TYPE: [&str; 7] = ["Cows", "A2", "Almond", "Soy",  "Oat", "Lactose Free", "None"];
+pub const TEMP_TYPE: [&str; 5] = ["Iced", "Meh", "Warm", "Hot", "Extra hot"];
+pub const SUGAR_TYPE: [&str; 4] = ["None", "One", "Two", "Three"];
